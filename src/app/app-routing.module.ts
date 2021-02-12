c795caec2d60ca195fe4313078e07708a39a2b42
James Macmillan <james.macmillan@engineius.co.uk>
2021-02-12 10:25:28 +0000

    base component ready for demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "@nativescript/angular";
-
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { DemoComponent } from './demo/demo.component';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "", redirectTo: "/demo", pathMatch: "full" },
+    { path: "demo", component: DemoComponent },
 ];
 
 @NgModule({
